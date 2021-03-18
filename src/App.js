1193f14d7cfcf820582fc23fcff25ff1c5a4006e
hson <hson@hist.co.kr>
2021-03-19 08:52:22 +0900

    first create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Potato from './Potato';
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Hello</h1>
+      <Potato />
     </div>
   );
 }
