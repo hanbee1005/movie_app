535851a2f0353506e6b2c1c3c999b511ff621d85
hson <hson@hist.co.kr>
2021-03-22 09:25:39 +0900

    객체 리스트 컴포넌트로 전달하기, 매개변수 2개 받기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
-function Food({ fav }) {
+function Food({ name, picture }) {
   // console.log(props.fav);
-  return <h3>I love {fav}.</h3>
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} style={{width: 300, height: 300}}/>
+  </div>
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image: "https://koreana.or.kr/resource/image/sub/2017_spring/life6-1.png"
+  },
+  {
+    name: "Bibimbap",
+    image: "http://img.etoday.co.kr/pto_db/2019/07/600/20190726153503_1350707_1200_876.jpg"
+  },
+  {
+    name: "Donkatsu",
+    image: "https://www.koreatripguide.com/wp-content/uploads/2018/01/korean_donkatsu_with_salad-e1515117505943.jpg"
+  },
+  {
+    name: "Kimbap",
+    image: "https://www.thespruceeats.com/thmb/aPsmbaRgCtFLUTUFGXZBIAW93H8=/3797x2848/smart/filters:no_upscale()/kimbap-korean-sushi-rolls-2118795-Hero-5b7dbdd346e0fb00250718b8.jpg"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
     </div>
   );
 }
