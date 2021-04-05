11334c7e30f6bd88ec1be63ce4edd3ab8743c5cf
hson <hson@hist.co.kr>
2021-04-05 16:07:58 +0900

    MovieApp 만들기 위한 첫단계: 로딩 글씨 띄우고 componentDidMount에서 5초 후 상태 변경

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class App extends React.Component {
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-
-  // component life cycle
-  // mounting
-  componentDidMount() {
-    console.log("component rended");
+    isLoading: true,
+    movies: []
   }
-
-  // updating
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-
-  // unmounting
-  componentWillUnmount() {
-    console.log("Goodbye~!");
-  }
-
+  
   render() {
-    console.log("I'm rendering");
-    return <div>
-      <h1>The number is {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 5000);
   }
 }
 
